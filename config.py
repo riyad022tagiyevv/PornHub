d5ebb4dea7342ed0e7cbab1ce3c4474e1b814a61
riyad022tagiyevv <97640853+riyad022tagiyevv@users.noreply.github.com>
2023-03-10 00:51:18 +0400

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 import os
 
-TOKEN = os.environ.get("TOKEN", "")
-ARQ_API_KEY = os.environ.get("ARQ_API_KEY", "")
-REPO_BOT = os.environ.get("REPO_BOT", "https://github.com/aryazakaria01/PornHub")
-OWNER = os.environ.get("OWNER", "")
-BOT_NAME = os.environ.get("BOT_NAME", "")
+TOKEN = os.environ.get("TOKEN", "5920231354:AAEX1EU6rms-2-7lBSn1XKDGZ0ANEiO6boU")
+ARQ_API_KEY = os.environ.get("ARQ_API_KEY", "GNXWRF-OMFTSU-ANRPRB-CHZGJV-ARQ")
+REPO_BOT = os.environ.get("REPO_BOT", "https://github.com/riyad022tagiyevv/PornHub")
+OWNER = os.environ.get("OWNER", "RiyadAndMe")
+BOT_NAME = os.environ.get("BOT_NAME", "PornoXnRobot")
 UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "RiyadSupport")
